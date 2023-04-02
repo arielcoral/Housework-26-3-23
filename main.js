c9cfef55f18d2ed4e7630788b344fb536a1266d9
arielcoral <123407136+arielcoral@users.noreply.github.com>
2023-04-02 13:35:12 +0300

    initial commit

---
+++ main.js
@@ -0,0 +1,56 @@
+// משימה ראשונה
+
+// var x = Number(prompt('please Enter the length: '))
+// var y = Number(prompt('please Enter the with: '))
+
+// if(x !== y){
+//     console.log(x * y)
+// }
+// else{
+//     console.log(x * y + " square")   
+// }
+
+
+// משימה שניה
+// var x = Number(prompt('Please enter your year of birth: '))
+// var y = 2022
+// var z = y - x
+// console.log(z)
+
+// משימה שלישית
+
+// var Arr = []
+
+// for(var i = 0; i < 6; i++){
+//     var Product = prompt('please Enter one product: ')
+//     Arr.push(Product)
+// }
+// Arr.reverse()
+// console.log(Arr)
+
+// משימה רביעית
+
+// var x = Number(prompt('please enter a number: '))
+
+// if(x > 7 || x <= 0){
+//     console.error("error!")
+// }
+// else if(x <= 5){
+//     console.log("Workday")
+// }
+// else{
+//     console.log("Weekend")
+// }
+
+// מישמה חמישית
+
+
+// var Arr2 = []
+
+// for(var i = 0; i < 6; i++){
+//     var Product = prompt('please Enter one product: ')
+//     Arr2.push(Product)
+// }
+// for (var index = 1; index < 6; index += 2) {
+//     console.log(Arr2[index]);
+// }
